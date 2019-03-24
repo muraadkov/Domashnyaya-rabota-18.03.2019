2f99f5ba97deebbdd7f75d98c18f25c0dc4fd1ad
Адылканов Муратжан Талгатович <@corp.itstep>
2019-03-24 17:51:00 +0600

    Домашняя работа

---
+++ DZ1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type console = typeof(Console);
+            MethodInfo[] methods = console.GetMethods();
+            Console.WriteLine("Список методов класса Console: \n");
+            foreach (MethodInfo m in methods)
+            {
+                Console.Write(m.ReturnType.Name + "\t" + m.Name);
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DZ2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DZ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Muratzhan",
+                Age = 16,
+                City = "Nursultan"
+            };
+            Type type = typeof(Person);
+            Console.WriteLine("Тип: " + type.Name);
+            PropertyInfo[] properties = type.GetProperties();
+            Console.WriteLine("Методы: ");
+            foreach (var property in properties) {
+                    Console.WriteLine(property.PropertyType + ":\t" + property.Name);
+            }
+            Console.Read();
+        }
+    }
+}
